a7ee48fb0f273c25abfc16b9bdc1e421f8c0ba2c
Anthony Boschat <AnthonyBoschat.dev@hotmail.com>
2023-09-23 12:59:07 +0200

    Ajout des style pour les categories

--- To-Do List/index.php
+++ To-Do List/index.php
@@ -1,6 +1,5 @@
 <?php 
 require "/home/parkser/Travail/global_tools.php";
-session_start();
 $user_name = recup_username_from_database();
 ?>
 <!DOCTYPE html>
@@ -8,6 +7,9 @@ $user_name = recup_username_from_database();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;1,500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="style.css">
     <title>To-Do List</title>
@@ -23,7 +25,12 @@ $user_name = recup_username_from_database();
 
     <main>
         <section id="categorie_box">
-
+            <div>
+                <span class="categorie">Lecture</span>
+                <span class="categorie">Travail</span>
+                <span class="categorie">Programmation</span>
+                <span id="add_categorie">(Ajouter une catégorie)</span>
+            </div>
         </section>
 
         <section id="list_box">
